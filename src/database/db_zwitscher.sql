24aa21f9f491f48baf30413902cbd7aca4655e38
D4rthB4n3 <dessel.bane@gmx.de>
2017-05-04 10:29:19 +0200

    created Database

---
+++ src/database/db_zwitscher.sql
@@ -0,0 +1,18 @@
+USE db_zwitscher;
+
+CREATE TABLE Users(
+  UserId INT NOT NULL AUTO_INCREMENT,
+  Username VARCHAR(255) NOT NULL UNIQUE ,
+  Password VARCHAR(255) NOT NULL ,
+  PRIMARY KEY (UserId)
+);
+
+CREATE TABLE Tweets(
+  TweetId INT NOT NULL AUTO_INCREMENT,
+  Content VARCHAR(180),
+  Date DATETIME NOT NULL ,
+  UserId INT,
+
+  PRIMARY KEY (TweetId),
+  FOREIGN KEY (UserId) REFERENCES Users(UserId)
+);
