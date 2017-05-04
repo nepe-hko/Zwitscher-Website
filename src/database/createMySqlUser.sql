d7fd66b79766d2e13a3c8260101af6a552e2f530
D4rthB4n3 <dessel.bane@gmx.de>
2017-05-04 11:09:43 +0200

    created basic database scripts

---
+++ src/database/createMySqlUser.sql
@@ -0,0 +1,3 @@
+CREATE USER 'zwitscher'@'%' IDENTIFIED BY 'zwitscher';
+GRANT ALL PRIVILEGES ON *.* TO 'zwitscher'@'%';
+FLUSH PRIVILEGES ;
---
+++ src/database/seedData.sql
@@ -0,0 +1,3 @@
+USE db_zwitscher;
+
+INSERT INTO users (UserId,Username,Password) VALUES (1,'zwitscher','zwitscher');
