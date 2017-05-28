ba6711672f6f108ff0a43655e03fa63a8c6eec25
Wadim <schaeferwa52753@th-nuernberg.de>
2017-05-28 20:34:07 +0200

    Login

---
+++ src/checkinformation.php
@@ -0,0 +1,27 @@
+﻿  <html>
+  <head>
+    <title>Login</title>
+  </head>
+  <body>
+  <?php
+
+
+	require './database/database.php';
+	$conn = createDatabaseConnection();
+
+    $users = $_POST["in_username"]; 
+    $password = $_POST["in_password"];
+
+
+
+	$okuser = $conn->query("SELECT Username FROM users WHERE Username = '".$_POST['in_username']."'");
+	$okpassword = $conn->query("SELECT Password FROM users WHERE Password = '".md5($_POST['in_password'])."'");
+
+    if ($users == $okuser && $password == $okpassword) {
+      include "./main.php" ;
+    } else {
+      echo "Falsche Eingabe." ;
+    }
+  ?>
+  </body>
+  </html>
--- src/login.php
+++ src/login.php
@@ -24,14 +24,14 @@
                 <img src="media/bird.png" width="100" height="100" />
             </a>
 
-            <form action="./main.html" target="_self">
+            <form method="POST" action="./checkinformation.php" target="_self">
                 <label id="lbl_username">Benutzername:</label>
                 <input id="in_username" type="text" name="in_username">
 
                 <label id="lbl_password">Passwort: </label>
                 <input id="in_password" type="password" name="in_password">
 
-                <input id="btn_submit" type="submit" value="Anmelden">
+                <input id="btn_submit" name=submit type="submit" value="Anmelden">
 
                 <a href="404.html" id="a_forgotPassword">Passwort vergessen?</a>
                 <p id="p_register">
@@ -39,34 +39,10 @@
                     Sie sich noch heute!
                 </p>
             </form>
-
-            <?php
-			
-            require './database/database.php';
+			<?php
+			require './database/database.php';
 			$conn = createDatabaseConnection();
-
-					session_start();
- 
-					if(isset($_GET['login'])) {
-					 $username = $_POST['username'];
-					 $password = $_POST['password'];
- 
-					 $statement = $pdo->prepare("SELECT * FROM users WHERE username = :username");
-					 $result = $statement->execute(array('username' => $username));
-					 $user = $statement->fetch();
- 
-					 //Überprüfung des Passworts
-					 if ($user !== false && password_verify($password, $user['password'])) {
-						 $_SESSION['userid'] = $user['id'];
-							die('Login erfolgreich. Weiter zur <a href="main.php">Hauptseite</a>');
-							} else {
-							$errorMessage = "Benutzername oder Passwort war ungültig";
-					 }
-						if(isset($errorMessage)) {
-						 echo $errorMessage;
-						}
-					}
-					?>
+			?>
 
         </section>
     </main>
